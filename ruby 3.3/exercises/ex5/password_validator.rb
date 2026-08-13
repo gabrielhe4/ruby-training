fd326a5144f5a614577811bde86d9bcc9439cf9d
Gabriel Hdez <gabrielhdezro@gmail.com>
2026-08-13 17:53:38 -0600

    Adding emojis to password validator example

--- ruby 3.3/exercises/ex5/password_validator.rb
+++ ruby 3.3/exercises/ex5/password_validator.rb
@@ -11,19 +11,27 @@ puts "Pick a password"
 password = gets.chomp.to_s
 
 if password.length < 8
-   puts "Password must be at least 8 characters long."
+  puts "❌ Password must be at least 8 characters long."
+else
+  puts "✅ Contains at least 8 characters long"
 end
 
 if not password.match?(/[a-z]/)
-  puts "Password must contain lowercase letters."
+  puts "❌ Password must contain lowercase letters."
+else
+  puts "✅ Contains at least one lowercase letter."
 end
 
 if not password.match?(/[A-Z]/)
-  puts "Password must contain an uppercase letter."
+  puts "❌ Password must contain an uppercase letter."
+else 
+  puts "✅ Contains at least one uppercase letter."
 end
 
 if not password.match?(/[0-9]/)
-  puts "Password must contain a number."
+  puts "❌ Password must contain a number."
+else
+  puts "✅ Contains at least one number."
 end
 
 
