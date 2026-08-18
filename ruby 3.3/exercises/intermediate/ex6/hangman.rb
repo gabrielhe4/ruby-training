fe1a0c8a5890a88f34369cee3b1b5e54e1ecfc01
Gabriel Hdez <gabrielhdezro@gmail.com>
2026-08-17 18:28:25 -0600

    creating files for hangman exercise

---
+++ ruby 3.3/exercises/intermediate/ex6/hangman.rb
@@ -0,0 +1,8 @@
+=begin
+Juego del ahorcado (Hangman)
+
+    Lee una lista de palabras desde un archivo o usa un array predefinido.
+    Muestra el progreso con guiones bajos (_ _ _ _).
+    Lleva cuenta de intentos fallidos y letras ya usadas.
+    Dibuja (con texto ASCII) el progreso del ahorcado.
+=end
